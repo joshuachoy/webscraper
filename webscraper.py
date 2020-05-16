57aec72f2091707bf51a9394556a86de0172f428
Joshua <42399915+joshuachoy@users.noreply.github.com>
2020-05-16 14:51:27 +0800

    Create webscraper.py

---
+++ webscraper.py
@@ -0,0 +1,19 @@
+import urllib
+import ssl
+import requests
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+url = 'https://www.gic.com.sg/news-and-resources/pgim-and-gic-help-cios-build-balanced-portfolios-when-investing-in-illiquid-private-assets/'
+
+#connect to website
+uClient = uReq(url)
+page_html = uClient.read()
+uClient.close()
+
+#print header
+page_soup = soup(page_html,'html.parser')
+print(page_soup.h1.text)
+
+#test
+print('test')
